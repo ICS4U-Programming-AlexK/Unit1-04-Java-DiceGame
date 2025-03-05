ee299aa7e2b03fb64db7428e44e0635d1e3f6e3a
Alex Kapajika <alex.kapajika.ntumba@stu.ocsb.ca>
2025-03-05 15:11:46 +0000

    DiceGame Programm

--- DiceGame.java
+++ DiceGame.java
@@ -5,13 +5,24 @@ import java.util.Scanner;
  * @author  Alex Kapajika
  * @version 1.0
  * @since   2025-03-03
+ * @
  */
-
-public class DiceGame {
-    public static void main(String[] args) {
+public final class DiceGame {
+    /**
+     * @exception IllegalStateException
+     * @see IllegalStateException
+     */
+    private DiceGame() {
+        throw new IllegalStateException("Utility class");
+    }
+    /**
+     * @param args Unused.
+     */
+    public static void main(final String[] args) {
         Random random = new Random();
         Scanner scanner = new Scanner(System.in);
-        int numberToGuess = random.nextInt(6) + 1; // Generates a number between 1 and 6
+        // Generates a number between 1 and 6
+        int numberToGuess = random.nextInt(6) + 1;
         int numberOfGuesses = 0;
         int userGuess = 0;
         boolean correctGuess = false;
@@ -21,17 +32,20 @@ public class DiceGame {
                 userGuess = scanner.nextInt();
                 numberOfGuesses++;
                 if (userGuess < 1 || userGuess > 6) {
-                    System.out.println("Please enter a number between 1 and 6.");
+                System.out.println("Please enter a number between 1 and 6.");
                 } else if (userGuess == numberToGuess) {
                     correctGuess = true;
-                    System.out.println("You guessed the right number: " + numberToGuess);
-                    System.out.println("It took you " + numberOfGuesses + " guess(es)");
+                System.out.println("You guessed the right number: ");
+                System.out.println(numberToGuess);
+                System.out.println("It took you "
+                + numberOfGuesses + " guess(es)");
                 } else {
                     System.out.println("Incorrect guess, try again!");
                 }
             } catch (InputMismatchException e) {
-                System.out.println("This input is not a number. Please enter a valid number between 1 and 6.");
-                scanner.next(); // Clear invalid input
+            System.out.println("This input is not a number.");
+            System.out.println("Please enter a valid number between 1 and 6.");
+            scanner.next(); // Clear invalid input
             }
         }
         scanner.close();
--- package-info.java
+++ package-info.java
@@ -1,7 +1,5 @@
 /**
- * 
  * This is a package info
- * 
  * @author  Alex Kapajika
  * @version 1.0
  * @since   2025-03-2
